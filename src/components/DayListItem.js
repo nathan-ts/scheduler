c1111f74bcc83cc1398f45b796dafffab1fd6604
nathan-ts <42843617+nathan-ts@users.noreply.github.com>
2022-04-19 21:51:50 -0400

    update daylistitem for spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,10 +8,14 @@ export default function DayListItem(props) {
     'day-list__item--full': props.spots === 0 ? true : false
   });
 
+  const spots = props.spots === 0 ? 
+    'no spots remaining' : 
+    props.spots === 1 ? `1 spot remaining` : `${props.spots} spots remaining`;
+
   return (
     <li className={listClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{spots}</h3>
     </li>
   );
 }
