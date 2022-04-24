f43d8d307ab34477c7651873516ae7a299605655
nathan-ts <42843617+nathan-ts@users.noreply.github.com>
2022-04-23 23:09:35 -0400

    update form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,19 +4,35 @@ import "./styles.scss";
 import Header from "components/Appointment/Header.js";
 import Empty from "components/Appointment/Empty.js";
 import Show from "components/Appointment/Show.js";
+import Form from "components/Appointment/Form.js";
 
+import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
 
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show 
+      {/* {props.interview ? <Show 
         student={props.interview.student} 
         interviewer={props.interview.interviewer} 
-      /> : <Empty />}
-      {/* {props.time ? `Appointment at ${props.time}` : 'No Appointments'} */}
+      /> : <Empty />} */}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && <Form interviewers={[]} onCancel={() => transition(EMPTY)} />}
     </article>
   );
 }
